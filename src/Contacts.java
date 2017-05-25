016e1b67f394b7c9d824a5994345f4fa2b6701e2
rcortezk9 <rcortezk9@gmail.com>
2017-05-25 14:46:36 -0500

    created list array and had it write to txt file for loop to have each contact display

--- src/Contacts.java
+++ src/Contacts.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by renecortez on 5/25/17.
@@ -9,7 +11,7 @@ import java.nio.file.Paths;
 public class Contacts {
     public static void main(String[] args) throws IOException {
         String directory = "data";
-        String filename = "contacts.txt";
+        String filename = "contact.txt";
 
         Path dataDirectory = Paths.get(directory);
         Path dataFile = Paths.get(directory, filename);
@@ -20,5 +22,14 @@ public class Contacts {
         if (Files.notExists(dataFile)) {
             Files.createFile(dataFile);
         }
+
+        List<String> contactList = Arrays.asList("Tuesday|2105555555", "Rene|2106458394", "John Doe|2236475746", "Jane Doe|2344325643");
+        Files.write(Paths.get("data", "contact.txt"), contactList);
+
+        List<String> lines = Files.readAllLines(dataFile);
+        for (String line : lines){
+            System.out.println(line);
+        }
     }
+
 }
